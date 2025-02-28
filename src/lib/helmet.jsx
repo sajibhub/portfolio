4f686c6362c105a0c9137a29a692af7cce3173f0
sajib-bd <mohammadsajib996@gmail.com>
2025-02-28 16:27:14 +0600

    add keyword and meta taf etc more for seo

--- src/lib/helmet.jsx
+++ src/lib/helmet.jsx
@@ -10,21 +10,21 @@ const HelmetSEO = () => {
             <Helmet>
                 <title>{` ${Path} | MOHAMMAD SAJIB`}</title>
                 <meta name="description" content="MOHAMMAD SAJIB - A skilled full-stack developer specializing in backend development, JavaScript, and React, based in Khagrachari, Bangladesh." />
-                <meta name="keywords" content="MOHAMMAD SAJIB, Web Developer, Backend Developer, Full-Stack Developer, JavaScript, React, Node.js, Express.js, MongoDB, Khagrachari" />
+                <meta name="keywords" content="Mohammad Sajib, Web Developer,SAJIB, Backend Developer, Full-Stack Developer, JavaScript, React, Node.js, Express.js, MongoDB, Khagrachari, MERN Stack,MERN stack developer in Khagrachari" />
                 <meta name="author" content="MOHAMMAD SAJIB" />
 
                 <meta property="og:title" content={`${Path} | MOHAMMAD SAJIB`} />
                 <meta property="og:description" content="Expert in backend and full-stack development, specializing in JavaScript, React, and Node.js. Based in Khagrachari, Bangladesh." />
-                <meta property="og:image" content="https://sajib.xyz/src/assets/images/profile.png" />
-                <meta property="og:url" content="https://sajib.xyz/" />
+                <meta property="og:image" content="../assets/images/profile.png" />
+                <meta property="og:url" content="https://sajib.xyz" />
                 <meta property="og:type" content="website" />
 
                 <meta name="twitter:title" content={`${Path} | MOHAMMAD SAJIB`} />
                 <meta name="twitter:description" content="Expert in backend and full-stack development, specializing in JavaScript, React, and Node.js. Based in Khagrachari, Bangladesh." />
-                <meta name="twitter:image" content="https://sajib.xyz/src/assets/images/profile.png" />
+                <meta name="twitter:image" content="../assets/images/profile.png" />
                 <meta name="twitter:card" content={SummaryImage} />
 
-                <link rel="canonical" href="https://sajib.xyz/" />
+                <link rel="canonical" href="https://sajib.xyz" />
             </Helmet >
 
         </>
